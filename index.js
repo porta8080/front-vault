9f43846aee38efe6acd12e7d8031c55ef3b22b9a
porta8080 <adm.porta8080@gmail.com>
2016-12-05 01:02:12 -0200

    First commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function DataVaultCookie(){
+  this.storage = localStorage;
+}
+
+function DataVaultSession(){
+  this.storage = sessionStorage;
+}
+
+function DataVault(){
+  this.cookie = new DataVaultCookie();
+  this.session = new DataVaultSession();
+}
+
+DataVault.prototype.set = function(key,value){
+  this.storage.setItem(key,value);
+};
+
+DataVault.prototype.get = function(key){
+  return this.storage.getItem(key);
+};
+
+DataVault.prototype.has = function(key){
+  return key in this.storage;
+};
+
+DataVault.prototype.remove = function(key){
+  this.storage.removeItem(key);
+};
+
+DataVault.prototype.clear = function(){
+  this.storage.clear();
+};
+
+DataVaultCookie.prototype = Object.create(DataVault.prototype);
+DataVaultSession.prototype = Object.create(DataVault.prototype);
+
+module.exports = new DataVault();
---
+++ teste.js
@@ -0,0 +1,1 @@
+var vault = require('./index');
